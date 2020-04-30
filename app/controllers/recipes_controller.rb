0220813002351b3b55868167dfebea50e3e05c3b
Yuri Espinosa <yuriespinosapires@outlook.com>
2020-04-30 20:31:14 -0300

    Usando partials na view edit e new

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,7 +13,6 @@ class RecipesController < ApplicationController
   end
 
   def update
-    recipe_params = params.require(:recipe).permit(:name, :stuff, :calories, :prepare_mode, :cost)
     @recipe = Recipe.find(params[:id])
     @recipe.update(recipe_params)
     redirect_to @recipe
@@ -24,7 +23,6 @@ class RecipesController < ApplicationController
   end
 
   def create
-    recipe_params = params.require(:recipe).permit(:name, :stuff, :calories, :prepare_mode, :cost)
     @recipe = Recipe.new(recipe_params)
     @recipe.save
     redirect_to @recipe
@@ -34,6 +32,12 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
     @recipe.destroy
     redirect_to recipes_url
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :stuff, :calories, :prepare_mode, :cost)
   end
 
 end
