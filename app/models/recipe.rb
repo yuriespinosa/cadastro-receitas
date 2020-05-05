3436024ca489d49649319564222f70250cc8cd34
Yuri Espinosa <yuriespinosapires@outlook.com>
2020-05-05 17:39:40 -0300

    Criando validadores nos modelos

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,14 @@
 class Recipe < ApplicationRecord
 
+  validates :name, :stuff, :duration, :prepare_mode, presence:true
+  validates_length_of :stuff, maximum:200
+  validates_length_of :prepare_mode, maximum:200
+  validates_numericality_of :calories, greather_than_or_equal_to:0, presence:true
+  validates_format_of :poster, with: /\w+\.(gif|png|jpg)\z/i, message: " utilizar somente arquivos válidos"
+  
+  KIND = %w(Carnes Aves Massas Saladas Doces)
+  validates_inclusion_of :kind, in: KIND
+
   def light?
     calories < 100
   end
@@ -7,4 +16,5 @@ class Recipe < ApplicationRecord
   def self.created_at
     where('created_at <= ?', Time.now).order('created_at desc')
   end
+
 end
